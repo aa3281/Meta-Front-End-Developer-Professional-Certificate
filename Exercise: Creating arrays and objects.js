dd6133177b8382769f9109061fe55a7919fc70d0
aa3281 <adinaaless@gmail.com>
2022-07-23 18:20:03 +0800

    Exercise: Creating arrays and objects

---
+++ Exercise: Creating arrays and objects.js
@@ -0,0 +1,16 @@
+var clothes = [];
+clothes.push("pants");
+clothes.push("shirt");
+clothes.push("jacket");
+clothes.push("socks");
+clothes.push("hat");
+clothes.pop();
+clothes.push("new clothing");
+
+console.log(clothes[2]);
+
+var favCar = {};
+favCar.color ="red";
+favCar.convertible = true;
+
+console.log(favCar);
