cb628942ecca5d68e4f78954057e7414872abff1
aa3281 <adinaaless@gmail.com>
2022-07-23 14:28:37 +0800

    Declaring Variables Exercise

---
+++ Exercise: Declaring variables.js
@@ -0,0 +1,14 @@
+var petDog = 'Rex';
+var petCat = 'Pepper';
+var catSound = 'purr';
+var dogSound = 'woof';
+
+console.log(petDog);
+console.log("My pet dog's name is: " + petDog);
+console.log("My pet cat's name is: " + petCat);
+console.log(petDog, "says", dogSound);
+console.log(petCat, " says", catSound);
+catSound = "meow";
+console.log(petCat, "now says", catSound);
+
+return petDog
