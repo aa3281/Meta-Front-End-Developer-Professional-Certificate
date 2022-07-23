c5be25eebde70896a42fcbfa8afe25815eefbcc1
aa3281 <adinaaless@gmail.com>
2022-07-23 18:38:05 +0800

    Error Prevention

---
+++ Exercise: Error prevention.js
@@ -0,0 +1,19 @@
+function addTwoNums(a,b) {
+	try {
+		if(typeof(a) != "number") {
+			throw new ReferenceError("The first argument is not a number");
+		}
+		else if(typeof(b) != "number") {
+			throw new ReferenceError("The second argument is not a number");
+		}
+		else {
+			console.log(a+b);
+		}
+	}
+	catch(err) {
+		console.log("Error!", err);
+	}
+};
+
+addTwoNums(5, "5");
+console.log("It still works");
