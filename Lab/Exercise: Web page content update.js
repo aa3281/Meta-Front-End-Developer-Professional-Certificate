3eb3f5f30396690672cf74099995eb851f0e6f13
aa3281 <adinaaless@gmail.com>
2022-07-25 19:10:52 +0800

    Exercise: Web page content update

---
+++ Lab/Exercise: Web page content update.js
@@ -0,0 +1,9 @@
+var h1 = document.createElement('h1')
+h1.innerText = "Type into the input to make this text change"
+
+var input = document.createElement('input')
+input.setAttribute('type', 'text')
+
+document.body.innerText = '';
+document.body.appendChild(h1);
+document.body.appendChild(input);
