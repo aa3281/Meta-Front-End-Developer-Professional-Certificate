5cb08c2e29d8236d5aba6b7952cd45e2ce7c5806
aa3281 <adinaaless@gmail.com>
2022-07-25 20:46:31 +0800

    Writing a Unit Test

---
+++ Lab/Writing a Unit Test/jest-testing/timesTwo.js
@@ -0,0 +1,8 @@
+// Task 1: Code the timesTwo function declaration
+
+function timesTwo(num) {
+    return num * 2
+}
+// Task 2: Export the timesTwo function as a module
+
+module.exports = timesTwo;
---
+++ Lab/Writing a Unit Test/jest-testing/timesTwo.test.js
@@ -0,0 +1,7 @@
+const timesTwo = require('./timesTwo');
+
+// Write the first test
+
+test("returns the number times 2", () => {
+    expect(timesTwo(10)).toBe(20);
+});
