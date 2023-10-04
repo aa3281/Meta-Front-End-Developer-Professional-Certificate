49d35764a4babe94eb5bf8945d4ee69c3c8ebdae
GamakichyOnly <vdlei49@gmail.com>
2023-10-04 10:57:07 +0800

    minor changes

--- Programming with Javascript/Testing/addFive.js
+++
@@ -1,5 +0,0 @@
-function addFive(val) {
-	return val + 5;
-};
-
-module.exports = addFive;
--- Programming with Javascript/Testing/addFive.test.js
+++
@@ -1,7 +0,0 @@
-const { default: TestRunner} = require("jest-runner");
-const addFive = require(`./addFive`);
-
-test('return the number plus 5', () => {
-	expect(addFive(1)).toBe(6);
-
-})
